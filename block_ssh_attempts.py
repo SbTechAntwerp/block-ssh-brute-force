d4bb69a37cf1e93c564886603206ebc2cb6d538c
OsScripting <sb@localhost.localdomain>
2021-06-02 01:51:31 -0700

    added 2 modules to remove the not  Invalid user and group by session id

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
-from os import linesep
-
-
+import re
+#read the log file
 with open('./sshdlog') as sshdlog:
-    if "dis" in sshdlog:
-        print(sshdlog + "disconnected")
-    else: 
-        print(sshdlog + "not disconnected")
+    readsshdlog = sshdlog.read()
+    splitsshdlog = readsshdlog.splitlines()
+#test the splitted lines 
+# print(splitsshdlog[0])
+def remove_unnecessary_lines(array):
+    array_with_invalidUser = []
+    for line in array:
+        if "Invalid user" in line:
+            array_with_invalidUser.append(line)
+    return(array_with_invalidUser)
 
-'''
-        for line in sshdlog:
-            if line == '*dis*':
-                print(line + "disconnected")
-            
-            else:
-                print(line + "not disconnected")
-                '''
+def filter_ips_by_session_id(array):
+    array_of_ips = []
+    current_session_id = ''
+    for line in array:
+        session = re.findall(r'\[\d+\]', line)[0]
+        ip= 
