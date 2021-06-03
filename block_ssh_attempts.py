5ca8301a39cd6800307579e9f0ed63cff87e0f11
OsScripting <sb@localhost.localdomain>
2021-06-03 16:03:35 -0700

    Added a lot of the options
    ++ still some work but it's in progress

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,22 +1,28 @@
 '''
-read file 
-make list from file
+This script takes a file and checks if the ip address appears at least 3 times then it blockes that IP in the firewall
 
-find line with invalid user
-
-filter Invalid user,group session, copy ip address to list, 
-if ip adrees sx3 in list then call pwblock ip
-
-for loop sesion id to current session id then send ip adrees to ip address array
-if session id same as current then continue else newsession to current session id and add ip to array
+-h = print a short help text and the script will 'not' be executed.
+-v will print for each IP address the 'IP address + blocking'  that would be blocked to the screen .
+-d prints 'debug' output, default no output is shown.
+-n read the log file(s) but will not call block_ip()
+other option = the script will wrtite the help to stderr and stop immediately.
+other args are files [default is ./sshdlog]
+Catch exceptions . If a file cannot be read, print a message to stderr and move on to the next file.
 '''
 #!/usr/bin/python3
+from block_ssh_attempts1 import block_user
 import re
 import fwblock
-#read the log file
-with open('./sshdlog') as sshdlog:
-    readsshdlog = sshdlog.read()
-    splitsshdlog = readsshdlog.splitlines()
+import getopt
+import sys
+
+
+script_args = sys.argv[1:] 
+
+option_string = 'dhvn'
+opts, extra_args = getopt.getopt(script_args, option_string)
+
+
 #test the splitted lines 
 # print(splitsshdlog[0])
 def remove_unnecessary_lines(array):
@@ -45,21 +51,100 @@ def filter_ips_by_session_id(array):
             array_of_ips.append(ip)
     return(array_of_ips)
 
-def block_user(array):
+def bad_ips(array):
     '''
-    Block all IP address that tried at least 3 times. 
+    Returns all IP address that tried at least 3 times. 
     '''
-    ips_to_block = list(set([x for x in array if array.count(x) > 2]))
-    for x in ips_to_block:
+    return list(set([x for x in array if array.count(x) > 2]))
+
+def block_ips(array, printIPS = False):
+    '''
+
+    '''
+    for x in array:
         fwblock.block_ip(x)
-        print(x)
+        if printIPS:
+            print("blocking" + x)
+    
     
 
+# block_ips(
+#     bad_ips(
+#         filter_ips_by_session_id(
+#             remove_unnecessary_lines(
+#                 splitsshdlog
+#     ))))
+
+
+
+#settings
+block_users = True
+show_help_text = False
+print_bad_ips = False
+print_debug = False
+
+#!/usr/bin/python3
+# If -h option doesn't exist, make sure your script doesn't contain debug prints.
+
+
+for option, option_arg in opts:
+    if option == '-h':
         
+        # break
+        show_help_text = True
+    elif option == '-v':
+        print_bad_ips = True
+    elif option == '-d':
+        print_debug =True
+    elif option == "-n":
+        block_users = False
+    # else option == '-'
+    #     print('\t', sys.argv[0], ' [-d][-f <filename>][-h]', sep='')
+    #     sys.exit(0) # don't execute if user doesn't know how to use this script
 
-block_user(filter_ips_by_session_id(remove_unnecessary_lines(splitsshdlog)))
 
 
-        
+files = []
+if extra_args == []:
+    files.append("./sshdlog")
+else:
+    files = extra_args
+
+for x in files:
+#read the log file
+    with open(x) as sshdlog:
+        readsshdlog = sshdlog.read()
+        splitsshdlog = readsshdlog.splitlines()
+    if show_help_text:
+        #the same as the help text of the module.
+        print("This script takes a file and checks if the ip address appears at least 3 times"
+        +"then it blockes that IP in the firewall\n-h = print a short help text and the script"
+        +"will 'not' be executed.\n-v will print for each IP address the 'IP address + blocking'"
+        +" that would be blocked to the screen .\n-d prints 'debug' output, default no output is shown."
+        +"\n-n read the log file(s) but will not call block_ip()\nother option = the script will wrtite "
+        +"the help to stderr and stop immediately.\nother args are files [default is ./sshdlog]\nCatch exceptions ."
+        +"If a file cannot be read, print a message to stderr and move on to the next file.")
+        break
+    elif print_bad_ips and block_users:
+        block_ips(
+            bad_ips(
+                filter_ips_by_session_id(
+                    remove_unnecessary_lines(
+                        splitsshdlog
+            ))), True)
+    elif print_bad_ips and  not block_users:
+        print(
+            bad_ips(
+                filter_ips_by_session_id(
+                    remove_unnecessary_lines(
+                        splitsshdlog
+            ))))
+    elif not print_bad_ips and  not block_users:
+        bad_ips(
+            filter_ips_by_session_id(
+                remove_unnecessary_lines(
+                    splitsshdlog
+        )))
+
 
 
